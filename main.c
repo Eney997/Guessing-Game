fdd32ef3800dec5a0d0479892125836c54092041
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2024-02-18 13:01:18 +0400

    first commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int secretNumber = 5;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outofGuesses = 0;
+
+    while(guess!= secretNumber && outofGuesses == 0){
+        if(guessCount<guessLimit){
+           printf("Enter a number: ");
+           scanf("%ddsa",&guess);
+           guessCount++;
+        }else{
+        outofGuesses = 1;
+        }
+    }
+    if(outofGuesses==1){
+        printf("Out of guesses,Try again");
+    }else{
+        printf("Hey you win!");
+    }
+
+    return 0;
+}
